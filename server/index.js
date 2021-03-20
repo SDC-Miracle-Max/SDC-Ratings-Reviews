c86059e0faf7df855f62dc9513e5ec9e12b3753d
bryancWA <bryanclark@Bryans-MacBook-Pro.local>
2021-03-20 16:04:10 -0700

    successful implementation of reivews/sort api and reviews/meta api

--- server/index.js
+++ server/index.js
@@ -11,11 +11,11 @@ app.use(morgan('dev'));
 
 app.use(express.json());
 
-app.get('/api/getTwoReviews', (req, res) => {
-    const { id } = req.params;
-    console.log('id logged:', id);
-    query.getTwoReviews(req, res);
-});
+// app.get('/api/getTwoReviews', (req, res) => {
+//     const { id } = req.params;
+//     console.log('id logged:', id);
+//     query.getTwoReviews(req, res);
+// });
 
 app.get('/reviews/sort/:id/:sort', (req, res) => {
   const { id, sort } = req.params;
@@ -32,50 +32,13 @@ app.get('/reviews/sort/:id/:sort', (req, res) => {
 
 })
 
+app.get('/reviews/meta/:id', (req, res) => {
+    const { id } = req.params;
+    query.getMetaData(id, res);
+})
+
 
 
 app.listen(PORT, () => {
     console.log(`server listening on localhost://${PORT}`)
 })
-
-
-// REVIEWS
-// app.get('/reviews/sort/:id/:sort', (req, res) => {
-//     const { id, sort } = req.params;
-//     const lowerSort = sort.toLowerCase();
-//     const reviewsURL = `${options.url}/reviews/?product_id=${id}&count=100&sort=${lowerSort}`;
-//     axios
-//       .get(reviewsURL, options)
-//       .then(({ data }) => res.send(data.results))
-//       .catch((err) => console.log(err));
-//   });
-  
-//   app.get('/reviews/meta/:id', (req, res) => {
-//     const { id } = req.params;
-//     const metaReviewsURL = `${options.url}/reviews/meta/?product_id=${id}`;
-//     axios
-//       .get(metaReviewsURL, options)
-//       .then(({ data }) => res.send(data))
-//       .catch((err) => console.log(err));
-//   });
-  
-//   app.put('/reviews/:review_id/helpful', (req, res) => {
-//     const { review_id } = req.params;
-//     axios.put(`${options.url}/reviews/${review_id}/helpful`, { body: { review_id } }, options)
-//       .then(() => res.send(204))
-//       .catch(console.log);
-//   });
-  
-//   app.post('/reviews', (req, res) => {
-//     axios.post(`${options.url}/reviews`, req.body, options)
-//       .then(() => res.send(204))
-//       .catch(console.log);
-//   });
-  
-//   app.put('/reviews/:review_id/report', (req, res) => {
-//     const { review_id } = req.params;
-//     axios.put(`${options.url}/reviews/${review_id}/report`, { body: { review_id } }, options)
-//       .then(() => res.send(204))
-//       .catch(console.log);
-//   });
-  
--- server/queries.js
+++ server/queries.js
@@ -2,68 +2,156 @@ const config = require('../config/dbConfig.js')
 const { Pool } = require('pg');
 const pool = new Pool (config);
 
-const getTwoReviews = (req, res) => {
-    pool.query('SELECT * FROM review WHERE id < 3', (err, results) => {
-        if (err) {
-            console.log('error in getTwoReviews', err);
-            res.sendStatus(500);
-        }
-        res.json(results.rows);
-    })
-}
+// const getTwoReviews = (req, res) => {
+//     pool.query('SELECT * FROM review WHERE id < 3', (err, results) => {
+//         if (err) {
+//             console.log('error in getTwoReviews', err);
+//             res.sendStatus(500);
+//         }
+//         res.json(results.rows);
+//     })
+// }
 
 const sortNewest = (id, res) => {
-  pool.query(`SELECT * FROM review WHERE product_id = ${id}`, (err, results) => {
-      if (err) {
-          console.log('err in sortNewest', err);
-          res.sendStatus(500);
-      } else {
-          const sorted = results.rows.sort((a, b) => {
-              return b.post_data - a.post_date;
-          })
-          res.json(sorted);
-      }
-  })
+  
+    const text = `SELECT * FROM review WHERE product_id = $1`;
+    const values = [id];
+  pool.query(text, values)
+    .then((results) => {
+      noReportedArr = results.rows.filter((review) => review.reported === false);
+      const sorted = noReportedArr.sort((a, b) => {
+        return a.post_data - b.post_date;
+      })
+      res.json(sorted)
+    })
+    .catch((err) => {
+      console.log('err in newest', err);
+      res.sendStatus(500);
+    });
+      
+  
 }
 
 const sortHelpful = (id, res) => {
-  pool.query(`SELECT * FROM review WHERE product_id = ${id}`, (err, results) => {
-    if (err) {
-        console.log('err in sortHelpful', err);
-        res.sendStatus(500);
-    } else {
-        console.log('helpful sort', results.row);
-        const sorted = results.rows.sort((a, b) => {
-            return b.helpfulness - a.helpfulness;
+  const query = {
+    text: `SELECT * FROM review WHERE product_id = $1`,
+    values: [ id ]
+  }
+  pool
+    .query(query)
+    .then((results) => {
+        noReportedArr = results.rows.filter((review) => review.reported === false);
+        const sorted = noReportedArr.sort((a, b) => {
+        return b.helpfulness - a.helpfulness;
         })
-        res.json(sorted);
-    }
-  })
+        res.json(sorted)
+        })
+    .catch((err) => {
+      console.log('err in sortHelpful', err);
+      res.sendStatus(500);
+    });
 }
 
 const sortRelevant = (id, res) => {
-  pool.query(`SELECT * FROM review WHERE product_id = ${id}`, (err, results) => {
-    if (err) {
-        console.log('err in sortRelevant', err);
-        res.sendStatus(500);
-    } else {
+  const query = {
+      text: `SELECT * FROM review WHERE product_id = $1`,
+      values: [ id ]
+  }
+  pool
+    .query(query)
+    .then((results) => {
         let relevant = [];
-        results.rows.forEach((review) => {
-          if (review.recommend === true) {
-             relevant.push(review) 
-          }
+        relevantArr = results.rows.filter((review) => review.reported === false);
+        relevantArr.forEach((review) => {
+            if (review.recommend === true) {
+                relevant.push(review);
+            }
         })
         const sorted = relevant.sort((a, b) => {
             return b.helpfulness - a.helpfulness;
         })
         res.json(sorted);
+    })
+    .catch((err) => {
+        console.log('err in sortRelevant', err);
+        res.sendStatus(500);
+    });
+}
+
+const getMetaData = (id, res) => {
+    rAndRquery = {
+        text: 'Select recommend, rating FROM review WHERE product_id = $1',
+        values: [id]
     }
-  })
+    pool.query(rAndRquery.text, rAndRquery.values)
+      .then((results) => {
+        let resultsObj = {
+            ratings: {
+                1: 0,
+                2: 0,
+                3: 0,
+                4: 0,
+                5: 0
+            },
+            recommend: {
+                0: 0
+            }
+        };
+         results.rows.forEach((review) => {
+             if (review.recommend) {
+                 resultsObj.recommend[0]++;
+             }
+             if (review.rating === 1) {
+                 resultsObj.ratings[1]++;
+             }
+             if (review.rating === 2) {
+                resultsObj.ratings[2]++;
+            }
+            if (review.rating === 3) {
+                resultsObj.ratings[3]++;
+            }
+            if (review.rating === 4) {
+                resultsObj.ratings[4]++;
+            }
+            if (review.rating === 5) {
+                resultsObj.ratings[5]++;
+            }
+         })
+        return resultsObj;
+      })
+      .then((results) => {
+        let resultsObj = results;
+        query = {
+            text: `SELECT c.character_name, cr.char_value, cr.characteristic_id FROM characteristic c
+            INNER JOIN characteristic_review cr ON c.id = cr.characteristic_id WHERE product_id = $1`,
+            values: [id]
+        }
+        pool.query(query.text, query.values)
+            .then((results) => {        
+                let characteristics = {};
+                results.rows.forEach((row) => {
+                    if (characteristics[row.character_name]) {
+                    characteristics[row.character_name].value = (characteristics[row.character_name].value + row.char_value) / 2;
+                    } else {
+                        characteristics[row.character_name] = {};
+                        characteristics[row.character_name].id = row.characteristic_id;
+                        characteristics[row.character_name].value = row.char_value;
+                    }
+                })
+                resultsObj.characteristics = characteristics;
+                res.send(resultsObj)
+                })
+        })
+      .catch((err) => {
+          console.log('err in getMeta', err);
+          res.sendStatus(500);
+      })
 }
 
 module.exports = {
-    getTwoReviews,
+    // getTwoReviews,
     sortHelpful,
     sortRelevant,
-    sortNewest
+    sortNewest,
+    getMetaData
 }
